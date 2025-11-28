b9d0f30cdf5af58627721fc2c875b2382f566cf7
Edson Silva-Júnior <edsonbbiologia@gmail.com>
2025-11-28 11:41:48 -0300

    Add files via upload

--- analises_consultoria_11.2025.R
+++ analises_consultoria_11.2025.R
@@ -72,6 +72,33 @@ ramos
 
 area <- upgma_data$label
 
+area
+
+### Dendograma ----
+
+ggplot() +
+  geom_segment(data = ramos, aes(x = x, y = y,
+                                 xend = xend, yend = yend),
+               linewidth = 1) +
+  labs(x = NULL,
+       y = "Similçaridade de Bray-Curtis") +
+  scale_x_continuous(breaks = 1:4,
+                     labels = agrup_hie$labels$label) +
+  scale_y_continuous(expand = FALSE) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1,
+                                   color = "black", size = 17.5),
+        axis.text = element_text(color = "black", size = 17.5),
+        axis.title = element_text(color = "black", size = 17.5),
+        title = element_text(color = "black", size = 17.5),
+        panel.grid.major.y = element_line(linewidth = 1, color = "gray80"),
+        panel.grid.minor.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        panel.grid.minor.x = element_blank()) +
+  ggview::canvas(height = 10, width = 12)
+
+ggsave(filename = "upgma.png", height = 10, width = 12)
+
 # Curva de rarefação ----
 
 # Diversidade taxonômica ----
