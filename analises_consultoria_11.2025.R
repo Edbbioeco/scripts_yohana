f75825df3b5652f273b0e57b1b6768f730e736c4
Edson Silva-Júnior <edsonbbiologia@gmail.com>
2025-11-28 18:01:34 -0300

    Add files via upload

--- analises_consultoria_11.2025.R
+++ analises_consultoria_11.2025.R
@@ -226,6 +226,7 @@ matriz_whit |>
   ggplot(aes(Rank, Abundancia)) +
   geom_line(linewidth = 1) +
   scale_x_continuous(breaks = seq(1, 16, 2)) +
+  scale_y_continuous(breaks = seq(1, 10, 1)) +
   facet_wrap(~ Área, scales = "free", ncol = 2) +
   theme_classic() +
   theme(axis.text = element_text(color = "black", size = 25),
